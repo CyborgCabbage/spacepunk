5cf62920be2ce3d96f7ff27ac0cd9f6d7d93ae9b
CyborgCabbage <finlaylaidlaw9@gmail.com>
2022-03-20 18:44:10 +0000

    Tweak PlanetProperties

--- src/main/java/cyborgcabbage/spacepunk/util/PlanetProperties.java
+++ src/main/java/cyborgcabbage/spacepunk/util/PlanetProperties.java
@@ -7,18 +7,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public record PlanetProperties(float gravity, boolean hasAtmosphere) {
-    private static Map<Identifier, PlanetProperties> map = new HashMap<>() {{
+    private static final Map<Identifier, PlanetProperties> map = new HashMap<>() {{
         put(Spacepunk.MOON.getValue(), new PlanetProperties(0.17f, false));
         put(Spacepunk.VENUS.getValue(), new PlanetProperties(0.17f, true));
     }};
     public static PlanetProperties DEFAULT_PROPERTIES = new PlanetProperties(1.0f, true);
 
-    public float getGravity() {
-        return gravity;
-    }
-
     public static float getGravity(Identifier dimId) {
-        return map.getOrDefault(dimId, DEFAULT_PROPERTIES).getGravity();
+        return map.getOrDefault(dimId, DEFAULT_PROPERTIES).gravity();
     }
 
     public static boolean hasAtmosphere(Identifier dimId) {
